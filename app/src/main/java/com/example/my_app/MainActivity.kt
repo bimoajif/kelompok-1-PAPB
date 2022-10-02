07f7aeeec605d0341aff61d3a1107e447e2a0300
Diah Ayu Widyati <78640159+ayuwidyatidiah85@users.noreply.github.com>
2022-10-02 23:03:35 +0700

    activity update

--- app/src/main/java/com/example/my_app/MainActivity.kt
+++ app/src/main/java/com/example/my_app/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    // onCreate activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
