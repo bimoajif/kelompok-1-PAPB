799e66220200248d7a5a1456eaf988631642c978
bimoajif <bimoajifajrianto@gmail.com>
2022-10-02 14:05:10 +0700

    menambahkan fragment lifecycle

---
+++ tugas_layout_dan_navigasi/app/src/main/java/com/example/papb_tugas1/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.papb_tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import timber.log.Timber
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Timber.plant(Timber.DebugTree())
+        Timber.i("onCreate Called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Timber.i("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Timber.i("onResume Called")
+        Toast.makeText(applicationContext,"onResume Called",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Timber.i("onPause Called")
+        Toast.makeText(applicationContext,"onPause Called",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Timber.i("onDestroy Called")
+        Toast.makeText(applicationContext,"onDestroy Called",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Timber.i("onRestart Called")
+    }
+}
