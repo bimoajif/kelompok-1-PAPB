7bc6adc3906abe748eb1109a97b11e5792d5ad75
Anas-Syahirul <anasyahirul@gmail.com>
2022-11-23 10:05:31 +0700

    add Landing UI

---
+++ app/src/main/java/com/example/diettrackerapp/LandingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.diettrackerapp
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diettrackerapp.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class LandingActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+
+    }
+}
